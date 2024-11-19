564c2e3c3cba3b0a7837cbdb44950af786c9789d
Ana Guzmán <71992795+Minombreesana@users.noreply.github.com>
2024-11-19 14:57:59 -0500

    Add files via upload

---
+++ sensores de proximidad/sketch_oct13a/sketch_oct13a.ino
@@ -0,0 +1,27 @@
+const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
+const int Echo = 3;   //Pin digital 3 para el Echo del sensor
+ 
+void setup() {
+  Serial.begin(9600);//iniciailzamos la comunicación
+  pinMode(Trigger, OUTPUT); //pin como salida
+  pinMode(Echo, INPUT);  //pin como entrada
+  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+}
+ 
+void loop()
+{
+ 
+  long t; //timepo que demora en llegar el eco
+  long d; //distancia en centimetros
+ 
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(10);          //Enviamos un pulso de 10us
+  digitalWrite(Trigger, LOW);
+  
+  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
+  d= t*0.034/2;          //escalamos el tiempo a una distancia en cm
+  
+  Serial.print(d);      //Enviamos serialmente el valor de la distancia
+  Serial.println();
+  delay(500);          //Hacemos una pausa de 100ms
+}
